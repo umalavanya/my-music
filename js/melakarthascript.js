ad04a2d71d733a51eceae9b91f36971120c7e913
umalavanya <umalavanya7@gmail.com>
2024-03-14 15:37:05 -0400

    dhruvatala

--- js/melakarthascript.js
+++ js/melakarthascript.js
@@ -1,17 +1,53 @@
 // Melakartha Ragas
+
+const chakra1 = ["Kanakangi", "Ratnangi", "Ganamurti", "Vanaspati", "Manavati", "Tanarupi"] ;
+const chakra2 = [ "Senavati", "Hanumatodi", "Dhenuka", "Natakapriya", "Kokilapriya", "Rupavati"] ;
+const chakra3 = ["Gayakapriya", "Vakulabharanam", "Mayamalavagowla", "Chakravakam","Suryakantam", "Hatakambari"] ;
+const chakra4 = ["Jhankaradhvani", "Natabhairavi", "Keeravani", "Kharaharapriya", "Gourimanohari","Varunapriya"] ;
+const chakra5 = ["Mararanjani", "Charukesi", "Saranganata", "Hemavati", "Dharmavati", "Naganandini"] ;
+const chakra6 = ["Yagapriya","Ragavardhini", "Gangeyabhusani", "Vagadheeswari", "Sulini", "Chalanata",] ;
+
+const chakra7 = ["Salagam", "Jalarnavam", "Jhalavarali","Navaneetam", "Pavani", "Raghupriya"] ;
+const chakra8 = ["Gavambodhi", "Bhavapriya", "Subhapantuvarali", "Shadvidhamargini","Suvarnangi", "Divyamani"] ;
+
+const chakra9 = ["Dhavalambari", "Namanarayani", "Kamavardhani", "Ramapriya", "Gamakakriya", "Viswambhari"] ;
+const chakra10 = ["Simsini", "Hamsanadam", "Dhenuka", "Natakapriya", "Kokilapriya", "Rupavati"] ;
+
+const chakra11 = [] ;
+const chakra12 = [] ;
+
+
 const ragas = [
-    "Kanakangi", "Ratnangi", "Ganamurti", "Vanascari", "Manavati", "Tanavari", "Senavati", "Hanumatodi", "Dhenuka", "Natakapriya", "Kokilapriya", "Rupavati", "Gayakapriya", "Vakulabharanam", "Mayamalavagaula", "Chakravakam", "Suryakantam", "Hatakambari", "Jhankaradhwani", "Natabhairavi", "Keeravani", "Kharaharapriya", "Gourimanohari", "Varunapriya", "Mararanjani", "Charukesi", "Saranganata", "Hemavati", "Dharmavati", "Naganandini", "Yagapriya", "Ragavardhini", "Gangeyabhusani", "Vagadheeswari", "Sulini", "Chalanata", "Salagam", "Jalarnavam", "Jhalavarali", "Navaneetam", "Pavani", "Raghupriya", "Gavambodhi", "Bhavapriya", "Subhapantuvarali", "Shadvidhamargini", "Suvarnangi", "Divyamani", "Dhavalambari", "Namanarayani", "Kamavardhani", "Ramapriya", "Gamakakriya", "Viswambhari", "Simsini", "Hamsanadam", "Dhenuka", "Natakapriya", "Kokilapriya", "Rupavati", "Gayakapriya", "Vakulabharanam", "Mayamalavagaula", "Chakravakam", "Suryakantam", "Hatakambari", "Jhankaradhwani", "Natabhairavi", "Keeravani", "Kharaharapriya", "Gourimanohari", "Varunapriya", "Mararanjani"
+    
+      
+    ,  
+    , , , 
+      
+    ,  
+    , , 
+    , "Gayakapriya", "Vakulabharanam", 
+    "Mayamalavagaula", "Chakravakam", "Suryakantam", "Hatakambari", "Jhankaradhvani", "Natabhairavi", "Keeravani", 
+    "Kharaharapriya", "Gourimanohari", "Varunapriya", "Mararanjani"
 ];
 
-// Function to generate the list of ragas
-function generateRagaList() {
-    const ragaList = document.getElementById("ragaList");
-    ragas.forEach(raga => {
-        const li = document.createElement("li");
-        li.textContent = raga;
-        ragaList.appendChild(li);
+// Function to generate the table of ragas
+function generateRagaTable() {
+    const ragaTableBody = document.querySelector("#ragaTable tbody");
+
+    ragas.forEach((raga, index) => {
+        const row = document.createElement("tr");
+
+        const indexCell = document.createElement("td");
+        indexCell.textContent = index + 1;
+        row.appendChild(indexCell);
+
+        const nameCell = document.createElement("td");
+        nameCell.textContent = raga;
+        row.appendChild(nameCell);
+
+        ragaTableBody.appendChild(row);
     });
 }
 
-// Call the function to generate the list
-generateRagaList();
+// Call the function to generate the table
+generateRagaTable();
